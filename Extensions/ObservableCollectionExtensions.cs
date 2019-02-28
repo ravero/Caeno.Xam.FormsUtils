5c51948998c4eace8e7152926ccbb46861fe4e74
Rafael Veronezi <ravero@outlook.com>
2019-02-28 18:45:33 -0300

    Added Observable Collection Extensions.

---
+++ Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FormsUtils.Extensions
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void ClearAndAdd<T>(this ObservableCollection<T> collection, IEnumerable<T> items) {
+            collection.Clear();
+            foreach (var item in items)
+                collection.Add(item);
+        }
+    }
+}
